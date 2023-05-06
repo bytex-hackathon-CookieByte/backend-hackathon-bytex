6a258c117666d7d048824c72cb06fcf3c53b4651
GeorgeDenisDuluta <george.duluta@yahoo.com>
2023-05-07 01:35:30 +0300

    ChallengeController update

--- jobyfier/src/main/java/ro/fiipractic/hackathon/jobyfier/controller/ChallengeController.java
+++ jobyfier/src/main/java/ro/fiipractic/hackathon/jobyfier/controller/ChallengeController.java
@@ -4,13 +4,19 @@ import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ro.fiipractic.hackathon.jobyfier.dto.request.ChallengeRequestDto;
+import ro.fiipractic.hackathon.jobyfier.dto.request.QuestionFixedRequestDto;
+import ro.fiipractic.hackathon.jobyfier.dto.request.QuestionOpenRequestDto;
 import ro.fiipractic.hackathon.jobyfier.dto.request.StageRequestDto;
 import ro.fiipractic.hackathon.jobyfier.dto.response.ChallengeResponseDto;
 import ro.fiipractic.hackathon.jobyfier.dto.response.StageResponseDto;
 import ro.fiipractic.hackathon.jobyfier.model.Challenge;
 import ro.fiipractic.hackathon.jobyfier.model.Stage;
+import ro.fiipractic.hackathon.jobyfier.model.question.FixedQuestion;
+import ro.fiipractic.hackathon.jobyfier.model.question.OpenQuestion;
+import ro.fiipractic.hackathon.jobyfier.model.question.Question;
 import ro.fiipractic.hackathon.jobyfier.service.ChallengeService;
 import ro.fiipractic.hackathon.jobyfier.service.CompanyService;
+import ro.fiipractic.hackathon.jobyfier.service.QuestionService;
 
 import java.util.List;
 import java.util.UUID;
@@ -21,11 +27,13 @@ public class ChallengeController {
 
     final private ChallengeService challengeService;
     final private CompanyService companyService;
+    private QuestionService questionService;
 
 
-    public ChallengeController(ChallengeService challengeService, CompanyService companyService) {
+    public ChallengeController(ChallengeService challengeService, CompanyService companyService, QuestionService questionService) {
         this.challengeService = challengeService;
         this.companyService = companyService;
+        this.questionService = questionService;
     }
 
     @PostMapping()
@@ -61,5 +69,29 @@ public class ChallengeController {
         Challenge challenge = challengeService.getChallengeById(challengeId);
         List<Stage> stages = challengeService.getStagesByChallengeId(challengeId);
         return challengeService.convertStageToDto(stages);
+    }
+    @PostMapping("/stages/questions//open")
+    public ResponseEntity<String> addQuestionOpen(@Valid @RequestBody QuestionOpenRequestDto questionOpenRequestDto){
+        OpenQuestion openQuestion = questionService.convertDtoToQuestionOpen(questionOpenRequestDto);
+        openQuestion.setStage(challengeService.findStageById(questionOpenRequestDto.getStageId()));
+        questionService.save(openQuestion);
+        return ResponseEntity.ok(openQuestion.getId().toString());
+    }
+    @PostMapping("/stages/questions/fixed")
+    public ResponseEntity<String> addQuestionFixed(@Valid @RequestBody QuestionFixedRequestDto questionFixedRequestDto){
+        FixedQuestion fixedQuestion = questionService.convertDtoToQuestionFixed(questionFixedRequestDto);
+        fixedQuestion.setStage(challengeService.findStageById(questionFixedRequestDto.getStageId()));
+        System.out.println(questionFixedRequestDto.getCorrectAnswer());
+
+        questionService.save(fixedQuestion);
+        return ResponseEntity.ok(fixedQuestion.getId().toString());
+    }
+    @GetMapping("/stages/questions")
+    public List<Question> getAllOpen(){
+        return questionService.getQuestions();
+    }
+    @GetMapping("/stages/questions/search")
+    public List<Question> getByStage(@RequestParam UUID stageId){
+        return questionService.getQuestionsByStage(stageId);
     }
 }
