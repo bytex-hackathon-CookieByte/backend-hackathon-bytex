43c0a2fcc5390c72f7a55b12c980a26e46ac4fb3
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2023-05-06 14:34:13 +0300

    setup

---
+++ jobyfier/src/main/java/ro/fiipractic/hackathon/jobyfier/JobyfierApplication.java
@@ -0,0 +1,13 @@
+package ro.fiipractic.hackathon.jobyfier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobyfierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobyfierApplication.class, args);
+	}
+
+}
---
+++ jobyfier/src/test/java/ro/fiipractic/hackathon/jobyfier/JobyfierApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.fiipractic.hackathon.jobyfier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobyfierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
