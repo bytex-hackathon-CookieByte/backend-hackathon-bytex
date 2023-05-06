1a5258efa2e27f2ce39af985424442dc29fd9e51
vitalie7bocicov <vitalie.bocicov99@gmail.com>
2023-05-06 23:19:13 +0300

    cors disabled

--- jobyfier/src/main/java/ro/fiipractic/hackathon/jobyfier/JobyfierApplication.java
+++ jobyfier/src/main/java/ro/fiipractic/hackathon/jobyfier/JobyfierApplication.java
@@ -2,10 +2,26 @@ package ro.fiipractic.hackathon.jobyfier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JobyfierApplication {
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.exposedHeaders("*")
+						.allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(JobyfierApplication.class, args);
 	}
